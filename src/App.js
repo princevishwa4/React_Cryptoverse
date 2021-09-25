360e1890c8dbf8e0d67e404c2f9324fac251ad6c
pirncevishwa4 <princestudy4@gmail.com>
2021-09-25 13:49:30 +0530

    cryptoverse app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
 import './App.css';
+
+import { Navbar, Homepage, Exchanges, Cryptocurrencies, CryptoDetails, News } from './components';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="navbar">
+        <Navbar />
       </header>
+      <main className="main">
+        <Layout>
+          <div className="routes">
+            <Switch>
+              <Route exact path="/"><Homepage /></Route>
+              <Route exact path="/exchanges"><Exchanges /></Route>
+              <Route exact path="/cryptocurrencies"><Cryptocurrencies /></Route>
+              <Route exact path="/crypto/:coinId"><CryptoDetails /></Route>
+              <Route exact path="/news"><News /></Route>
+            </Switch>
+          </div>
+        </Layout>
+        <footer className="footer">
+          <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>
+            Cryptoverse <br />
+            All rights reserved
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </footer>
+      </main>
     </div>
   );
 }
